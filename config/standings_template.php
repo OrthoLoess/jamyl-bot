1bfbc0e092bd06adab2fa21743bb0148d434364a
Ed Landamore <ortho@ratship.net>
2015-05-06 19:05:51 +0100

    Add template for standings config flie

---
+++ config/standings_template.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Temporary implementation of the standings list
+    |--------------------------------------------------------------------------
+    |
+    |   Will probably keep the holders list.
+    |
+    */
+
+    'holders' => [
+        'alliances' => [
+            //
+        ],
+    ],
+
+    'blues' => [
+        'alliances' => [
+            //
+        ],
+        'corporations' => [
+            //
+        ],
+    ],
+
+    'light-blues' => [
+        'alliances' => [
+            //
+        ],
+        'corporations' => [
+            //
+        ],
+    ],
+
+];
