90f3e38e5066b88947ddd84b15f1d70d223b6269
Unknown <ortho@ratship.net>
2019-12-30 13:20:23 +0000

    Add twittercard tag

--- resources/views/jbmap.blade.php
+++ resources/views/jbmap.blade.php
@@ -2,7 +2,9 @@
 <head>
     <title>Provi JB Map</title>
     <meta property="og:type" content="website" />
+    <meta property="og:title" content="Provi JB Map" />
     <meta property="og:image" content="http://apoc.space/wp-content/uploads/2019/12/jb-map.png" />
+    <meta name="twitter:card" content="summary_large_image" />
 </head>
 <body>
 <img src="http://apoc.space/wp-content/uploads/2019/12/jb-map.png">
