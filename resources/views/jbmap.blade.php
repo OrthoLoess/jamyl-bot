e8af7215a8d58d45e51fd9cf79ae4ed94ec9fd00
Unknown <ortho@ratship.net>
2019-12-30 13:15:30 +0000

    Add basic open graph tags

--- resources/views/jbmap.blade.php
+++ resources/views/jbmap.blade.php
@@ -1,6 +1,8 @@
 <html>
 <head>
     <title>Provi JB Map</title>
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="http://apoc.space/wp-content/uploads/2019/12/jb-map.png" />
 </head>
 <body>
 <img src="http://apoc.space/wp-content/uploads/2019/12/jb-map.png">
