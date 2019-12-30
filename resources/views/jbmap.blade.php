2030015844bbcad732fe1ff735c27dffb37224a4
Unknown <ortho@ratship.net>
2019-12-30 17:12:27 +0000

    php derp

--- resources/views/jbmap.blade.php
+++ resources/views/jbmap.blade.php
@@ -3,12 +3,12 @@
     <title>Provi JB Map</title>
     <meta property="og:type" content="website" />
     <meta property="og:title" content="Provi JB Map" />
-    <meta property="og:description" content="{{ description }}" />
-    <meta property="og:image" content="{{ jburl }}" />
+    <meta property="og:description" content="{{ $description }}" />
+    <meta property="og:image" content="{{ $jburl }}" />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="Provi JB Map" />
 </head>
 <body>
-<img src="{{ jburl }}">
+<img src="{{ $jburl }}">
 </body>
 </html>
